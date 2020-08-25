3c02daea1f001dfb651b5e5737ac521b0d35d2ff
Francisco Sousa <franciscoeds1987@hotmail.com>
2020-08-25 08:13:20 -0300

    Trabalhando com edição de dados - Programando o evento no botão editar

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,9 +3,22 @@ class UserController {
   constructor(formId, tableId) {
 
     this.formEl = document.getElementById(formId)
+
     this.tableEl = document.getElementById(tableId)
 
     this.onSubmit()
+
+    this.onEdit()
+
+  }
+
+  onEdit() {
+
+    document.querySelector("#box-user-update .btn-cancel").addEventListener("click", () => {
+
+      this.showPanelCreate();
+
+    })
 
   }
 
@@ -157,10 +170,18 @@ class UserController {
         <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
         <td>${Utils.dateFormat(dataUser.register)}</td>
         <td>
-            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>
     `
+
+    tr.querySelector(".btn-edit").addEventListener("click", () => {
+
+      console.log(JSON.parse(tr.dataset.user))
+
+      this.showPanelUpdate()
+
+    })
 
     this.tableEl.appendChild(tr)
 
@@ -168,9 +189,26 @@ class UserController {
 
   }
 
+  showPanelCreate() {
+
+    document.querySelector("#box-user-create").style.display = "block"
+
+    document.querySelector("#box-user-update").style.display = "none"
+
+  }
+
+  showPanelUpdate() {
+
+    document.querySelector("#box-user-create").style.display = "none"
+
+    document.querySelector("#box-user-update").style.display = "block"
+
+  }
+
   updateCount() {
 
     let numberUsers = 0;
+
     let numberAdmin = 0;
 
     [...this.tableEl.children].forEach(tr => {
