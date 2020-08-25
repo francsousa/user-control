5e99372d878e0cc90e82878fa918060bf86215f6
Francisco Sousa <franciscoeds1987@hotmail.com>
2020-08-25 08:58:12 -0300

    Trabalhando com edição de dados - Utilizando o laço for in

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -177,7 +177,27 @@ class UserController {
 
     tr.querySelector(".btn-edit").addEventListener("click", () => {
 
-      console.log(JSON.parse(tr.dataset.user))
+      let json = JSON.parse(tr.dataset.user)
+
+      let form = document.querySelector("#form-user-update")
+
+      for (let name in json) {
+
+        if (json.hasOwnProperty(name)) {
+
+          let field = form.querySelector("[name="+ name.replace("_", "") +"]")
+
+          if (field) {
+
+            if (field.type === 'file') continue
+
+            field.value = json[name]
+
+          }
+
+        }
+
+      }
 
       this.showPanelUpdate()
 
